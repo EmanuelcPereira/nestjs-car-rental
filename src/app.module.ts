66c8bdac189820c7fda6882535e77ad9dbfe0fb4
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-12-08 11:18:56 -0300

    chore: configure postgres on the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
-import { CarsModule } from './cars/cars.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CarsModule } from '@/cars/cars.module';
 @Module({
-  imports: [CarsModule],
+  imports: [
+    CarsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'pguser',
+      password: 'pgpassword',
+      database: 'rental-car',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
