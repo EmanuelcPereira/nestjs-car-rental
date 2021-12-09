5c14c2d9c71fcbdf5404e4cbd709c8ca9b68b559
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-12-09 10:43:02 -0300

    chore: add environmental variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,27 @@
+import { configValidationSchema } from '@/config.schema';
+import { CarsModule } from '@/cars/cars.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CarsModule } from '@/cars/cars.module';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: configValidationSchema,
+    }),
     CarsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'pguser',
-      password: 'pgpassword',
-      database: 'rental-car',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      })
     })
   ],
   controllers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
+import { AppModule } from '@/app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@/app.module';
 
 async function bootstrap () {
-  const port = 3000
   const logger = new Logger()
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
-  logger.log(`Application running on port ${port}`)
+  logger.log(`Application running on port ${process.env.PORT}`)
 }
 bootstrap();
