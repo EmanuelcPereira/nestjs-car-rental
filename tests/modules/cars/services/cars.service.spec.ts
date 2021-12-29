8bf3948df69c734eced0f0fc2323b00110a1cda7
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-12-29 13:56:20 -0300

    refactor: change CarService tests

--- tests/modules/cars/services/cars.service.spec.ts
+++ tests/modules/cars/services/cars.service.spec.ts
@@ -1,8 +1,8 @@
 import { CarsService } from '@/modules/cars/services/cars.service';
 import { CarsRepository } from '@/modules/cars/repositories/cars.repository';
-import { GetCarsFilterDto } from '@/modules/cars/dtos/get.cars.filter.dto';
 import { Test } from '@nestjs/testing';
 import faker from 'faker';
+import { Car } from '../../../../src/modules/cars/Entities/car.entity';
 
 let carsService: CarsService
 let carsRepository
@@ -18,88 +18,63 @@ const mockCarsRepository = () => ({
   reactivateCar: jest.fn()
 })
 
-const mockCars = () => ([{
-  id: faker.datatype.uuid(),
-  brand: faker.random.word(),
-  color: faker.random.word(),
-  licensePlate: faker.random.word(),
-  isDeleted: false
-},
-{
-  id: faker.datatype.uuid(),
-  brand: faker.random.word(),
-  color: faker.random.word(),
-  licensePlate: faker.random.word(),
-  isDeleted: false
-}])
+const allCars: Car[] = [
+  new Car({
+    id,
+    brand: faker.random.word(),
+    color: faker.random.word(),
+    licensePlate: faker.random.word(),
+    isDeleted: false
+  }),
+  new Car({
+    id: faker.datatype.uuid(),
+    brand: faker.random.word(),
+    color: faker.random.word(),
+    licensePlate: faker.random.word(),
+    isDeleted: false
+  })]
 
-const mockCar = () => ({
+const newCar = new Car({
   id,
-  brand: faker.random.word(),
-  color: faker.random.word(),
-  licensePlate: faker.random.word(),
-  isDeleted: false
+  brand: 'Ford',
+  licensePlate: 'ABC-1234',
+  color: 'Green',
+  isDeleted: false,
 })
+
+const updateCar = new Car({
+  id,
+  brand: 'Alfa Romeo',
+  licensePlate: 'DEF-5678',
+  color: 'Burro fugido',
+  isDeleted: false,
+})
+
 
 describe('CarsService', () => {
   beforeEach(async () => {
     const module = await Test.createTestingModule({
       providers: [
         CarsService,
-        { provide: CarsRepository, useFactory: mockCarsRepository }
+        {
+          provide: CarsRepository,
+          useValue: {
+            getCars: jest.fn().mockResolvedValue(allCars),
+            getCarById: jest.fn().mockResolvedValue(allCars[0]),
+            createCar: jest.fn().mockResolvedValue(newCar),
+            updateCar: jest.fn().mockResolvedValue(updateCar),
+            inactivateCar: jest.fn().mockResolvedValue({ affected: 1 }),
+            reactivateCar: jest.fn().mockResolvedValue({ affected: 1 })
+          }
+        }
       ]
     }).compile()
     carsService = module.get<CarsService>(CarsService)
     carsRepository = module.get<CarsRepository>(CarsRepository)
   })
 
-  describe('getCars', () => {
-    it('ensure carsService get all cars from repository', async () => {
-      const cars = mockCars()
-      carsRepository.getCars.mockResolvedValue(cars)
-      const filter: GetCarsFilterDto = {}
-      const result = await carsService.getCars(filter)
-      expect(result).toEqual(cars)
-    })
-  })
-
-  describe('getCarById', () => {
-    it('ensure carsService get car by Id from repository', async () => {
-      const car = mockCar()
-      carsRepository.getCarById.mockResolvedValue(car)
-      const result = await carsService.getCarById(id)
-      expect(result).toEqual(car)
-    })
-  })
-
-  describe('create', () => {
-    it('ensure carsService create car with correct values', async () => {
-      const car = mockCar()
-      carsRepository.createCar.mockResolvedValue(car)
-      const result = await carsService.create({
-        brand: faker.random.word(),
-        color: faker.random.word(),
-        licensePlate: faker.random.word()
-      })
-      expect(result).toEqual(car)
-    })
-  })
-
-  describe('inactivate', () => {
-    it('ensure carsService inactivate set isDeleted to true', async () => {
-      carsRepository.inactivateCar.mockResolvedValue({ affected: 1 })
-      expect(carsRepository.inactivateCar).not.toHaveBeenCalled()
-      await carsService.inactivate(id)
-      expect(carsRepository.inactivateCar).toHaveBeenCalled()
-    })
-  })
-
-  describe('restore', () => {
-    it('ensure carsService restore set isDeleted to false', async () => {
-      carsRepository.reactivateCar.mockResolvedValue({ affected: 1 })
-      expect(carsRepository.reactivateCar).not.toHaveBeenCalled()
-      await carsService.reactive(id)
-      expect(carsRepository.reactivateCar).toHaveBeenCalled()
-    })
+  it('should be defined', () => {
+    expect(carsRepository).toBeDefined()
+    expect(carsService).toBeDefined()
   })
 })
